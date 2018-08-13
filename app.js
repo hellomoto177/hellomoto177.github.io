1dd2aa9c5305066e59a0d7a50454c9a76b6f37e1
Shirokovskikh Sergey <hellomoto177@yandex.ru>
2018-08-13 11:09:08 +0500

    test

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 class Calculator extends React.Component {
   render() {
+    const id = fetch(`aoeunsth${id}`);
     return <h1>Hi there</h1>;
   }
 }
+
+const response = axios.get("url");
 
 ReactDOM.render(<Calculator />, document.querySelector("#app"));
